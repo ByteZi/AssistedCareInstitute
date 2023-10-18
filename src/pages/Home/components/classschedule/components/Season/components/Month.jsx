87b6dfac645c9827c0bf7b42cd656c4cda843504
Ederick Fernandez <edericklf@gmail.com>
2023-10-18 03:33:56 -0700

    season header weight ess

--- src/pages/Home/components/classschedule/components/Season/components/Month.jsx
+++ src/pages/Home/components/classschedule/components/Season/components/Month.jsx
@@ -4,7 +4,7 @@ const Month = ({month, days, k}) => {
     return (
         <div key={k} className="month-con flex space-between align-center">
 
-            <h2 className='month'>{month}</h2>
+            <h3 className='month'>{month}</h3>
             <div className="month-con-details flex column">
                 <h4 classsName="month-header">Weekends</h4>
                 <p className="month-days">{days[k]}</p>
